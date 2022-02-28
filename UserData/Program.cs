935e0a46c371dbee8fee0796ecca3ee1b04b76fe
kaspar muuli <kasparmuuli@gmail.com>
2022-02-28 19:45:49 +0200

    Control Flow

---
+++ UserData/Program.cs
@@ -0,0 +1,27 @@
+﻿  using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust;
+            //programm kuvab kasutaja vanust konsoolis;
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOFBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2022 - yearOFBirth;
+
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            //string interpolation
+
+            Console.WriteLine($"Oled {userAge} aastat vana");
+
+        }
+    }
+}
